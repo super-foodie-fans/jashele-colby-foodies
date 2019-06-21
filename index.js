cd3accf7232542d2dbf55650051d63cb13f2e669
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-20 22:02:22 -0500

    getting the slider working

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // home page JS
+
+// class Carousel{
+//     constructor(element){
+//         this.carouselElement = element
+//         this.imageNumber = this.carouselElement.dataset.img
+
+//         this.index = 0
+        
+//        let timing = setInterval(this.startShow, 1000);
+        
+//     }
+
+
+//     startShow() {
+//         this.carouselElement[this.index].className = 'image-noshow'
+
+//         this.index = (this.index+1)%this.carouselElement.length
+
+//         this.carouselElement[this.index].className = 'image-show'
+//     }
+// }
+
+// let allImages = document.querySelectorAll('.carousel .slide')
+
+// allImages.forEach(images => {
+//     return new Carousel(images)
+// })
+
+
+let allImages = document.querySelectorAll('.carousel .slide')
+
+let theIndex = 0
+
+let slideTiming = setInterval(slideShow, 6000)
+
+
+function slideShow(){
+    allImages[theIndex].className = 'slide image-noshow'
+    theIndex = (theIndex+1)%allImages.length
+    allImages[theIndex].classList = 'slide image-show'
+}
