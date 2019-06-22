da4869489a4bd8817a367db99c71d236a450b747
Jashele T <jashelet@Jashele.domain>
2019-06-21 20:03:01 -0700

    added menu functionality

---
+++ menu.js
@@ -0,0 +1,29 @@
+class Dropdown {
+    constructor(element) {
+      
+      // Assign this.element to the dropdown element
+      this.element = element.querySelector(".dropdown");
+  
+      
+      // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+      this.button = element.querySelector('.dropdown-button');
+      
+      // assign the reference to the ".dropdown-content" class found in the dropdown element
+      this.content = element.querySelector('.dropdown-content');
+      
+      // Add a click handler to the button reference and call the toggleContent method.
+      this.button.addEventListener('click', () => {
+        this.toggleContent();
+      })
+    }
+  
+    toggleContent() {
+      
+      // Toggle the ".dropdown-hidden" class off and on
+      this.content.classList.toggle('dropdown-hidden');
+    }
+  }
+  
+  
+  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
