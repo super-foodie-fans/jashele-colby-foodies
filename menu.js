a0aea665f34fa458194bdbd5a5cfc625ebdeca94
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-24 23:39:36 -0500

    Got the nav bar working

--- menu.js
+++ menu.js
@@ -31,19 +31,12 @@ class Dropdown {
 
 
   const toggleMenu = () => {
-    // Toggle the "menu--open" class on your menu refence. 
     menu.classList.toggle('menu--open')
   }
   
-  // Start Here: Create a reference to the ".menu" class
   const menu = document.querySelector('.menu');
-  // create a reference to the ".menu-button" class
+
   const menuButton = document.querySelector('.menu-button');
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  
-  menuButton.addEventListener('click', () =>{
-    toggleMenu()
-  })
 
+  const dropDownBut = document.querySelector('.dropdown-button')
 
-  let menuLines = document.querySelectorAll('menu-lines')
