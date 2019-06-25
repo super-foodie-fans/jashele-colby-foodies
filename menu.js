9ef562b5bfd5d7dd2b88b230311c6b67eb5a98c3
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-24 22:38:40 -0500

    added menu translate animations. next is js for button

--- menu.js
+++ menu.js
@@ -25,5 +25,25 @@ class Dropdown {
   }
   
   
-  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+
   let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
+
+
+  const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    menu.classList.toggle('menu--open')
+  }
+  
+  // Start Here: Create a reference to the ".menu" class
+  const menu = document.querySelector('.menu');
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector('.menu-button');
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  
+  menuButton.addEventListener('click', () =>{
+    toggleMenu()
+  })
+
+
+  let menuLines = document.querySelectorAll('menu-lines')
